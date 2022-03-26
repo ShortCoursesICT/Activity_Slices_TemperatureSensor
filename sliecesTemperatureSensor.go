1c11dec0c835c25a42eb23c24590a726d95d83da
ShortCoursesICT <ict.cet.shortcourses@gmail.com>
2022-03-26 22:33:58 +0800

    slices with temperature sensor

---
+++ sliecesTemperatureSensor.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	//slices template
+	//room1 := []float64{20, 21, 23, 25, 22}
+	//room2 := []float64{27, 23, 25, 20, 30, 24}
+	//room3 := []float64{22, 23, 24, 22}
+
+	//Insert code here
+}
